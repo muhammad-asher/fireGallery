c77cde3bb6b8dcd59303db8fd891edb1a8eaf834
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-10-12 22:52:24 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
