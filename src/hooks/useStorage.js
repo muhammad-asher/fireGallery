c6270f7cf44c7a5bcc7a0998d29392561517119a
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-10-12 23:30:16 +0500

    Update useStorage.js

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectStorage } from "../firebase/config";
+import { projectStorage, projectFirestore } from "../firebase/config";
 
 const useStorage = (file) => {
 	const [progress, setProgress] = useState(0);
@@ -9,22 +9,24 @@ const useStorage = (file) => {
 	useEffect(() => {
 		// references
 		const storageRef = projectStorage.ref(file.name);
+		const collectionRef = projectFirestore.collection("images");
 
 		storageRef.put(file).on(
 			"state_changed",
 			(snap) => {
-				let percentage = (snap.bytesTransfered / snap.totalBytes) * 100;
+				let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
 				setProgress(percentage);
 			},
 			(err) => {
 				setError(err);
-			}
-		),
+			},
 			async () => {
 				const url = await storageRef.getDownloadURL();
-				setUrl(url);
-			};
+			}
+		);
 	}, [file]);
-	return { progress, error, url };
+
+	return { progress, url, error };
 };
+
 export default useStorage;
